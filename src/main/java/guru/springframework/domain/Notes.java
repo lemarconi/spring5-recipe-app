32d5a3734a3da885fed17ba9b37ccced4654fbcc
lemarconi <leandro.marconi@gmail.com>
2020-08-28 10:55:09 -0300

    Project Lombok tentative an rollback

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -20,26 +20,30 @@ public class Notes {
     private String recipeNotes;
 
     public Long getId() {
-        return id;
+        return this.id;
+    }
+
+    public Recipe getRecipe() {
+        return this.recipe;
+    }
+
+    public String getRecipeNotes() {
+        return this.recipeNotes;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
     }
 
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
     public void setRecipeNotes(String recipeNotes) {
         this.recipeNotes = recipeNotes;
+    }
+
+    public String toString() {
+        return "Notes(id=" + this.getId() + ", recipe=" + this.getRecipe() + ", recipeNotes=" + this.getRecipeNotes() + ")";
     }
 }
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -42,82 +42,6 @@ public class Recipe {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getPrepTime() {
-        return prepTime;
-    }
-
-    public void setPrepTime(Integer prepTime) {
-        this.prepTime = prepTime;
-    }
-
-    public Integer getCookTime() {
-        return cookTime;
-    }
-
-    public void setCookTime(Integer cookTime) {
-        this.cookTime = cookTime;
-    }
-
-    public Integer getServings() {
-        return servings;
-    }
-
-    public void setServings(Integer servings) {
-        this.servings = servings;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getDirections() {
-        return directions;
-    }
-
-    public void setDirections(String directions) {
-        this.directions = directions;
-    }
-
-    public Byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(Byte[] image) {
-        this.image = image;
-    }
-
-    public Notes getNotes() {
-        return notes;
-    }
-
     public void setNotes(Notes notes) {
         this.notes = notes;
         notes.setRecipe(this);
@@ -129,27 +53,108 @@ public class Recipe {
         return this;
     }
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Integer getPrepTime() {
+        return this.prepTime;
+    }
+
+    public Integer getCookTime() {
+        return this.cookTime;
+    }
+
+    public Integer getServings() {
+        return this.servings;
+    }
+
+    public String getSource() {
+        return this.source;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getDirections() {
+        return this.directions;
+    }
+
     public Set<Ingredient> getIngredients() {
-        return ingredients;
+        return this.ingredients;
+    }
+
+    public Byte[] getImage() {
+        return this.image;
+    }
+
+    public Difficulty getDifficulty() {
+        return this.difficulty;
+    }
+
+    public Notes getNotes() {
+        return this.notes;
+    }
+
+    public Set<Category> getCategories() {
+        return this.categories;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
     }
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
     }
 
-    public Difficulty getDifficulty() {
-        return difficulty;
+    public void setImage(Byte[] image) {
+        this.image = image;
     }
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
     }
 
-    public Set<Category> getCategories() {
-        return categories;
-    }
-
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
+    }
+
+
+    public String toString() {
+        return "Recipe(id=" + this.getId() + ", description=" + this.getDescription() + ", prepTime=" + this.getPrepTime() + ", cookTime=" + this.getCookTime() + ", servings=" + this.getServings() + ", source=" + this.getSource() + ", url=" + this.getUrl() + ", directions=" + this.getDirections() + ", ingredients=" + this.getIngredients() + ", image=" + java.util.Arrays.deepToString(this.getImage()) + ", difficulty=" + this.getDifficulty() + ", notes=" + this.getNotes() + ", categories=" + this.getCategories() + ")";
     }
 }
